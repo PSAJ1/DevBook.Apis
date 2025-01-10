43992ffcafad16991d9a60ca35d40d039a8618c0
PSAJ1 <pankajshah2941999@gmail.com>
2025-01-10 09:41:15 +0530

    Adding auth-middleware for practice.

---
+++ auth-middlware/index.js
@@ -0,0 +1,5 @@
+function auth(req,res,next){
+    console.log(req);
+}
+
+module.exports= auth;
--- public/app.js
+++ public/app.js
@@ -1,11 +1,20 @@
 const express = require("express")
-
+const auth =require("../auth-middlware/index");
 const app = express()
-
-app.use("/test",(req,res)=>{
-    res.send("welcome to test");
+app.use("/",auth);
+app.get("/admin",(req,res,next)=>{
+    console.log("/ Route Handler 1");
+    //res.send("Hello");
+    next();
 });
-
+app.get("/users",(req,res,next)=>{
+    console.log("/User Route handler 2");
+    //res.send("Hello");
+});
+app.get("/admin/users",(req,res,next)=>{
+    console.log("/User Route handler 2");
+    //res.send("Hello");
+});
 app.listen(3000,()=>{
     console.log("Server is running on port 3000.")
 });
